cc92bda180969e5f7f35a869bb2243e61ca7ea32
Jamal Shahverdiev <jamal.shahverdiev@gmail.com>
2016-07-31 16:25:11 +0400

    changed in added termcolor to python-installer

--- python-installer.sh
+++ python-installer.sh
@@ -7,7 +7,7 @@ if [ "$OSBSD" == "FreeBSD" ] && [ "$FVER" = "10" ]
 then
     if [ -f /usr/local/bin/python3.4 ] && [ -f /usr/local/bin/python2.7 ]
     then
-        echo "You have installed Python2.7 and Python3.4"
+        echo "Python2.7 and Python3.4 already installed"
         exit 0
     else
         /usr/sbin/pkg install -y bash vim bash-completion
@@ -18,12 +18,14 @@ then
         /usr/local/bin/python3.4 -m pip install netmiko
         /usr/local/bin/python3.4 -m pip install pexpect
         /usr/local/bin/python3.4 -m pip install Jinja2
+        /usr/local/bin/python3.4 -m pip install termcolor
         /usr/local/bin/python2.7 -m ensurepip
         /usr/local/bin/python2.7 -m pip install --upgrade pip
         /usr/local/bin/python2.7 -m pip install fabric
         /usr/local/bin/python2.7 -m pip install netmiko
         /usr/local/bin/python2.7 -m pip install pexpect
         /usr/local/bin/python2.7 -m pip install Jinja2
+        /usr/local/bin/python2.7 -m pip install termcolor
     fi
 else
     echo "****************************"
@@ -36,7 +38,7 @@ if [ -f /etc/issue ] && [ -f /etc/redhat-release ]
 then
     if [ -f /usr/local/bin/python3.4 ] && [ -f /usr/local/bin/python2.7 ]
     then
-        echo "You have installed Python2.7 and Python3.4"
+        echo "Python2.7 and Python3.4 already installed"
         exit 0
     else
         yum -y groupinstall "Development tools"
@@ -48,7 +50,7 @@ then
         ./configure
         make && make altinstall
         echo "*************************************"
-        echo "Python2.7 already installed"
+        echo "Python2.7 successfully installed"
         echo "*************************************"
         cd /usr/src
         wget https://www.python.org/ftp/python/3.4.4/Python-3.4.4rc1.tgz
@@ -62,12 +64,14 @@ then
         /usr/local/bin/python3.4 -m pip install netmiko
         /usr/local/bin/python3.4 -m pip install pexpect
         /usr/local/bin/python3.4 -m pip install Jinja2
+        /usr/local/bin/python3.4 -m pip install termcolor
         /usr/local/bin/python2.7 -m ensurepip
         /usr/local/bin/python2.7 -m pip install --upgrade pip
         /usr/local/bin/python2.7 -m pip install fabric
         /usr/local/bin/python2.7 -m pip install netmiko
         /usr/local/bin/python2.7 -m pip install pexpect
         /usr/local/bin/python2.7 -m pip install Jinja2
+        /usr/local/bin/python2.7 -m pip install termcolor
     fi
 else
     echo "****************************"
